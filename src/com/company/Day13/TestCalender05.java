029b4326c2c8bac1cba2aefd9480a015b1cdbb45
le123u <63946085+le123u@users.noreply.github.com>
2020-07-28 09:56:13 +0800

    Calender练习题

---
+++ src/com/company/Day13/TestCalender05.java
@@ -0,0 +1,51 @@
+package com.company.Day13;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class TestCalender05 {
+    public static void main(String[] args) throws ParseException {
+//        Calendar calendar = Calendar.getInstance();
+        // 获取给定日历的最大可能值
+        // calendar.getActualMaximum();
+
+        // 编写一个方法 根据给定的字符串yyyy-MM获取当月的最后一天
+
+        String lastDayOfMonth = getLastDayOfMonth("2020-06");
+        System.out.println(lastDayOfMonth);
+
+        int lastDay = lastDay(2020, 7);
+        System.out.println(lastDay);
+
+    }
+
+    private static void lastDay(String s) {
+    }
+
+    public static String getLastDayOfMonth(String yearmonth) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
+        Date date = sdf.parse(yearmonth);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int lastDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        return "" + lastDay;
+
+    }
+
+    public static int lastDay(int year , int month){
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.clear(); // 清除所有日期数据
+
+        calendar.set(Calendar.YEAR,year);
+        calendar.set(Calendar.MONTH,month-1);
+
+        int lastDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        return lastDay;
+
+    }
+}
